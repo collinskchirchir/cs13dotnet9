79ca547472e82d31e6906b809a91eb295d386c14
Collins Chirchir <collinskchirchir@gmail.com>
2024-10-10 23:04:25 +0300

    chapter 02

---
+++ Chapter02/Vocabulary/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// #error version
+WriteLine($"Computer named {Env.MachineName} says \"No. \"");
+
+WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}");
