3287ab4cbebcae8b6a42421331821978d756521d
Collins Chirchir <collinskchirchir@gmail.com>
2024-10-14 17:48:30 +0300

    chapter 03: exploring operators

---
+++ Chapter03/Operators/Program.cs
@@ -0,0 +1,20 @@
+﻿# region Exploring unary operators
+
+int a = 3;
+int b = a++; // postfix operator ++ operator executes after assignment
+
+WriteLine($"a is {a}, b is {b}");
+
+int c = 3;
+int d = ++c;
+WriteLine($"c is {c}, d is {d}");
+# endregion
+
+# region Assignment operators
+
+int p = 6;
+p += 3; // equivalent to: p = p + 3;
+p -= 3; // equivalent to: p = p - 3;
+
+
+# endregion
